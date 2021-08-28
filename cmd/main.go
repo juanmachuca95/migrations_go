093994f53d69d26388dcd384bd5ffaffbf6bd9a8
Machuca Juan <machucajuangabriel@gmail.com>
2021-08-28 17:02:07 -0300

    update

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+	"fmt"
 )
 
 func main(){
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "<h1>Hola Mundo</h1>")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
