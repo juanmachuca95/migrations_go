9993618ab661c50e74bb27653b6a3452b3e81d2d
Machuca jUAN <machucajuangabriel@gmail.com>
2021-11-10 16:47:32 -0300

    actualización de sas

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -7,9 +7,39 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/juanmachuca95/migrations_go/api"
+
+	page "github.com/juanmachuca95/migrations_go/pages"
 )
 
 func main() {
+
+	var message, resource string
+	data := page.Page{}
+	data.Title = "Juan Machuca: " + message + " - Ha finalizado: " + resource
+
+	initial := "Usuarios"
+
+	data.Steps = []page.Step{
+		{Title: "Usuarios", Done: false, Resource: "/users", Key: "users"},
+		{Title: "Personas", Done: false, Resource: "/personas", Key: "users"},
+		{Title: "Administradores", Done: false, Resource: "/admins", Key: "users"},
+		{Title: "Sas", Done: false, Resource: "/sas", Key: "users"},
+		{Title: "Socios", Done: false, Resource: "/socios", Key: "users"},
+	}
+
+	for index, value := range data.Steps {
+		if value.Key == resource {
+			value.Done = false
+			data.Steps[index+1].Done = true
+		}
+	}
+
+	log.Println(data.Steps)
+
+	log.Println(initial)
+
+	log.Fatal("terminar")
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("No se han podido cargar las variables de entorno.")
